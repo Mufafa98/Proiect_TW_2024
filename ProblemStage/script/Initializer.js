220a844feebd57a64c7e053a948dc45efaed885c
Mufafa98 <marian.parlea2@gmail.com>
2024-04-12 18:29:54 +0300

    Problem solver stage and error

--- ProblemStage/script/Initializer.js
+++ ProblemStage/script/Initializer.js
@@ -16,17 +16,61 @@ export function init() {
 		for (let index = 0; index < inOutContainer.length; index++) {
 			const element = inOutContainer[index];
 			element.style.width = `${
-				Number.parseInt(getComputedStyle(solutionContainer).width) - 64
+				Number.parseInt(getComputedStyle(solutionContainer).width) - 32
 			}px`;
 			element.style.height = `${
-				Number.parseInt(getComputedStyle(solutionContainer).height) * 0.45 - 64
+				Number.parseInt(getComputedStyle(solutionContainer).height) * 0.45 - 32
 			}px`;
 		}
 		actionDiv.style.width = `${
-			Number.parseInt(getComputedStyle(solutionContainer).width) - 64
+			Number.parseInt(getComputedStyle(solutionContainer).width) - 32
 		}px`;
 		actionDiv.style.height = `${
-			Number.parseInt(getComputedStyle(solutionContainer).height) * 0.1 - 64
+			Number.parseInt(getComputedStyle(solutionContainer).height) * 0.1 - 32
 		}px`;
 	}
+
+	const buttons = document.querySelectorAll(".Button");
+	for (let index = 0; index < buttons.length; index++) {
+		const element = buttons[index];
+		element.style.height = getComputedStyle(actionDiv).height;
+		element.style.width = `${
+			Number.parseInt(getComputedStyle(actionDiv).width) / 4
+		}px`;
+	}
+}
+
+export function initPopUp() {
+	const background = document.getElementById("popUpContainer");
+	const popUp = document.getElementById("popUp");
+	const popUpStyle = getComputedStyle(popUp);
+	background.style.display = "block";
+
+	popUp.style.marginLeft = `${
+		(window.innerWidth -
+			(Number.parseInt(popUpStyle.width) +
+				Number.parseInt(popUpStyle.paddingLeft) +
+				Number.parseInt(popUpStyle.paddingRight))) /
+		2
+	}px`;
+	popUp.style.marginTop = `${
+		(window.innerHeight -
+			(Number.parseInt(popUpStyle.height) +
+				Number.parseInt(popUpStyle.paddingTop) +
+				Number.parseInt(popUpStyle.paddingBottom))) /
+		2
+	}px`;
+
+	const popUpButton = document.getElementById("difficultySelectorButton");
+	const popUpSelector = document.getElementById("difficultySelector");
+	const popUpText = document.getElementById("popUpText");
+	popUpButton.style.width = getComputedStyle(popUpText).width;
+	popUpSelector.style.width = getComputedStyle(popUpText).width;
+	popUpSelector.style.height = `${
+		Number.parseInt(getComputedStyle(popUpButton).height) / 2
+	}px`;
+}
+
+export function unInitPopUp() {
+	document.getElementById("popUpContainer").style.display = "none";
 }
